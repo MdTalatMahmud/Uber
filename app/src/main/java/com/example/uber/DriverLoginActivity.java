83279ddb98f19d65f41f10742dde048a0b945bab
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-03-30 23:42:43 +0600

    update 2

---
+++ app/src/main/java/com/example/uber/DriverLoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.uber;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DriverLoginActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPassword;
+    private Button mLogin, mRegistration;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+
+        mEmail=(EditText) findViewById(R.id.email);
+        mPassword=(EditText) findViewById(R.id.password);
+
+        mLogin=(Button) findViewById(R.id.login);
+        mRegistration=(Button) findViewById(R.id.registration);
+    }
+}
